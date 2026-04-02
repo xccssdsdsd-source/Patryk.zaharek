f1cea21bac50239fdf3c4d176bfb40395e288f09
hvrs321412 <kajetan.kosinski1@gmail.com>
2026-04-02 19:49:48 +0200

    dsds

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        cream: '#FAF8F5',
+        charcoal: '#2C2C2C',
+        brass: '#B8956A',
+        'warm-gray': '#E8E4DF',
+        ink: '#1A1A1A',
+      },
+      fontFamily: {
+        serif: ['Georgia', 'Times New Roman', 'serif'],
+        sans: ['system-ui', 'sans-serif'],
+      },
+    },
+  },
+};
